18dbc14a07f32b930b6611b37267c5c7d17b20c4
Golla Uday Kiran <140073498+Golla-UdayKiran@users.noreply.github.com>
2026-08-31 15:21:02 +0530

    Add empty wishlist message and shopping prompt

---
+++ wishlist.js
@@ -0,0 +1,5 @@
+document.querySelector('.wishlist-page-wishlist .vamtam-look-for-heart')
+  .innerHTML = 'YOUR WISHLIST IS EMPTY';
+
+document.querySelector('.wishlist-page-wishlist .vamtam-start-shopping')
+  .innerHTML = 'Start WishListing';
